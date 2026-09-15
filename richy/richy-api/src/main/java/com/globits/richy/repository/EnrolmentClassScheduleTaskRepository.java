89328ba3453f43385c06c9b7671f3a5eecc4365a
Nguyen Tung Long <longnt0712@gmail.com>
2026-09-15 18:29:10 +0700

    test

---
+++ richy/richy-api/src/main/java/com/globits/richy/repository/EnrolmentClassScheduleTaskRepository.java
@@ -0,0 +1,9 @@
+package com.globits.richy.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import com.globits.richy.domain.EnrolmentClassScheduleTask;
+
+@Repository
+public interface EnrolmentClassScheduleTaskRepository extends JpaRepository<EnrolmentClassScheduleTask, Long> {
+}
